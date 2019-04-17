9bbf052987e63ea69dee462f5e4da96ffae23ca2
Unknown <473070615@qq.com>
2019-04-17 17:25:05 +0800

    .

--- portfolio/views.py
+++ portfolio/views.py
@@ -4,4 +4,4 @@ from gallery.models import Gallery
 
 def home(request):
 	gallerys = Gallery.objects
-	return render(request, 'home.html', {'gallerys': gallerys})
+	return render(request, 'home.html', {'Gallerys': gallerys})
